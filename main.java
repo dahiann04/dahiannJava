80a19fcbcb6d05be27edb2cb26226fa2cbe6c45e
dahiann04 <20243ds121@utez.edu.mx>
2025-05-08 09:21:13 -0600

    si

---
+++ main.java
@@ -0,0 +1,11 @@
+public class Caracteres {
+    public static void main(String[] args) {
+        char letra = 'a';
+        char simbolo = '$';
+        char numeroChar = 'S';
+
+        System.out.println("letra = " + letra);
+        System.out.println("simbolo = " + simbolo);
+        System.out.println("numeroChar = " + numeroChar);
+    }
+}
